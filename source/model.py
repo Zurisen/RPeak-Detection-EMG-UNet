198a165bc54d4410f734d24eae814442dd7ad9d7
CMT <mtcarlos98@gmail.com>
2025-01-10 16:30:35 +0100

    adjusted encoder decoders

--- source/model.py
+++ source/model.py
@@ -28,20 +28,20 @@ class EncoderBlock(nn.Module):
         return x, p
 
 class DecoderBlock(nn.Module):
-    def __init__(self, in_channels):
+    def __init__(self, in_channels, out_channels):
         super(DecoderBlock, self).__init__()
-        self.out_channels = in_channels//2
-        self.upconv = nn.ConvTranspose1d(in_channels, self.out_channels, kernel_size=2, stride=2)
+        self.upconv = nn.ConvTranspose1d(in_channels, out_channels, kernel_size=2, stride=2)
         self.conv = nn.Sequential(
-            nn.Conv1d(in_channels, self.out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm1d(self.out_channels),
+            nn.Conv1d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm1d(out_channels),
             nn.LeakyReLU(0.25),
         )
         
     def forward(self, x, skip):
 
+        if skip is not None:
+            x = torch.cat((x, skip), dim=1)
         x = self.upconv(x)
-        x = torch.cat((x, skip), dim=1)
         x = self.conv(x)
         return x
 
@@ -49,35 +49,38 @@ class UNet_1D(nn.Module):
     def __init__(self):
         super(UNet_1D, self).__init__()
         self.encoder_block1 = EncoderBlock(1, 16)
-        self.encoder_block2 = EncoderBlock(16, 32)
-        self.encoder_block3 = EncoderBlock(32, 64)
-        self.encoder_block4 = EncoderBlock(64, 128)
+        self.encoder_block2 = EncoderBlock(16, 16)
+        self.encoder_block3 = EncoderBlock(16, 32)
+        self.encoder_block4 = EncoderBlock(32, 32)
+        self.encoder_block5 = EncoderBlock(32, 64)
+        self.encoder_block6 = EncoderBlock(64, 64)
         
-        self.bottleneck = Convolution(128, 256)
         
-        self.decoder_block1 = DecoderBlock(256)
-        self.decoder_block2 = DecoderBlock(128)
-        self.decoder_block3 = DecoderBlock(64)
-        self.decoder_block4 = DecoderBlock(32)
+        self.decoder_block1 = DecoderBlock(64, 64)
+        self.decoder_block2 = DecoderBlock(128, 32)
+        self.decoder_block3 = DecoderBlock(64, 32)
+        self.decoder_block4 = DecoderBlock(64, 16)
+        self.decoder_block5 = DecoderBlock(32, 16)
         
-        self.outconv = nn.Conv1d(16, 1, kernel_size=1)
+        self.outconv = nn.ConvTranspose1d(32, 1, kernel_size=2, stride=2)
         
     def forward(self, x):
-        skip1, p1 = self.encoder_block1(x)
-        skip2, p2 = self.encoder_block2(p1)
-        skip3, p3 = self.encoder_block3(p2)
-        skip4, p4 = self.encoder_block4(p3)
-
-        x = self.bottleneck(p4)
+        _, p1 = self.encoder_block1(x)
+        _, p2 = self.encoder_block2(p1)
+        _, p3 = self.encoder_block3(p2)
+        _, p4 = self.encoder_block4(p3)
+        _, p5 = self.encoder_block5(p4)
+        _, p6 = self.encoder_block6(p5)
         
-        x = self.decoder_block1(x, skip4)
-        x = self.decoder_block2(x, skip3)
-        x = self.decoder_block3(x, skip2)
-        x = self.decoder_block4(x, skip1)
+        x = self.decoder_block1(p6, None)
+        x = self.decoder_block2(x, p5)
+        x = self.decoder_block3(x, p4)
+        x = self.decoder_block4(x, p3)
+        x = self.decoder_block5(x, p2)
         
-        x = self.outconv(x)
+        x = self.outconv(torch.cat([x, p1], dim=1))
         
-        return torch.softmax(x, dim=1)
+        return torch.sigmoid(x)
     
     def initialize_weights_uniform(self, lower_bound=-0.1, upper_bound=0.1):
         for param in self.parameters():
